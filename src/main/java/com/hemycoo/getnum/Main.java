da103c3f2fffedc18069415ae31f6a3aed009a41
hemy5 <hl2396953683@163.com>
2022-01-26 16:57:20 +0800

    初始化数据

---
+++ src/main/java/com/hemycoo/getnum/Main.java
@@ -0,0 +1,11 @@
+package com.hemycoo.getnum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] firstRow = {1, 2, 3, 4, 5};
+        int[] secondRow = {6, 7, 8, 9, 10};
+        int[] thirdRow = {11, 12, 13, 14, 15};
+        int[] fourthRow = {16, 17, 18, 19, 20};
+        int[] fifthRow = {21, 22, 23, 24, 25};
+    }
+}
